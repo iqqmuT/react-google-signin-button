5650e2e318529f1cdd3c5b9686a22ce2a15be009
Tuomas Jaakola <tuomas.jaakola@iki.fi>
2024-06-07 15:07:03 +0300

    Extend ButtonProps from HTMLAttributes

--- src/Button.types.ts
+++ src/Button.types.ts
@@ -1,9 +1,7 @@
-export interface ButtonProps {
+export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   label?: string;
   theme?: 'light' | 'dark' | 'neutral';
   shape?: 'rectangular' | 'square' | 'pill' | 'circle';
   width?: string;
   logoAlign?: 'left' | 'center';
-  disabled?: boolean;
-  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
